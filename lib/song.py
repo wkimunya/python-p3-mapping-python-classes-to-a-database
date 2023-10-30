b81e4484ee21566e0941d60566f97de9cd34e157
wkimunya <136619749+wkimunya@users.noreply.github.com>
2023-10-30 13:21:57 +0300

    completed lab

--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,43 @@
 from config import CONN, CURSOR
 
 class Song:
-    pass
+
+    def __init__(self, name, album):
+        self.id = None
+        self.name = name
+        self.album = album
+
+    @classmethod
+    def create_table(cls):
+        sql = """
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY,
+                name TEXT,
+                album TEXT
+            )
+        """
+
+        CURSOR.execute(sql)
+
+    def save(self):
+        sql = """
+            INSERT INTO songs (name, album)
+            VALUES (?, ?)
+        """
+
+        CURSOR.execute(sql, (self.name, self.album))
+
+        # After executing the insert, get the last inserted row's ID
+        CURSOR.execute("SELECT last_insert_rowid() FROM songs")
+        self.id = CURSOR.fetchone()[0]
+
+        # Commit the transaction
+        CONN.commit()
+
+        return self.id
+
+    @classmethod
+    def create(cls, name, album):
+        song = Song(name, album)
+        song.save()
+        return song
